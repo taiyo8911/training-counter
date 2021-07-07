72fc24f8b1d698417b0a1014c3488918d00ca1c1
taiyo8911 <68884687+taiyo8911@users.noreply.github.com>
2021-07-07 19:36:57 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,55 +1,51 @@
+// カウントダウンの初期値を設定する
+// 初期値をnumに格納
+
+function send(num) {
+    document.getElementById('num').textContent = num;
+    input = num;
+    return input;
+}
+
+// カウンターを表示する
+function displayNum() {
+    no--;
+    document.getElementById('num').innerText = no;
+}
+
 // 1の位の音を鳴らす
-function play1(num) {
+function play() {
     let music = new Audio();
-    music.load();
-    music.src = `audio/${num}.mp3`;
+    music.src = `audio/${no}.mp3`;
     music.volume = 0.5; //音量の初期値(50%)
     music.play();
 }
 
-
 // 10の位の音を鳴らす
-let src = 0;
-
 function play10() {
     let music = new Audio();
     music.src = "audio/10-.mp3";
-    music.load();
     music.volume = 0.5; //音量の初期値(50%)
     music.play();
 
-    setTimeout(function play1 () {
-        music.src = `audio/${src}.mp3`;
+    setTimeout(function () {
+        music.src = `audio/${no-10}.mp3`;
         music.volume = 0.5; //音量の初期値(50%)
-        music.load();
         music.play();
     }, 400);
-
 }
 
-// カウントを表示する
-function displayNum(){
-    document.getElementById('num').innerText = num;
-}
+// 10より大きい数を読み上げる
+function over10() {
+    let count = 0; // 繰り返し数カウント用
+    let end = no - 11;  // 繰り返し回数
 
-
-let num = 0;
-let count = 0;
-let end = 10;  // 繰り返したい回数
-let interval = 1000; // 繰り返し処理の実行間隔（ミリ秒数 1000=1秒）
-
-function start() {
     let id = setInterval(
         function () {
-            // --- 繰り返したい処理をここに書く ---
-            num++;
-
             // カウントを表示する
             displayNum();
-
             // 音を鳴らす
-            play1(num);
-
+            play10();
             // 実行回数インクリメント
             count += 1;
 
@@ -57,36 +53,25 @@ function start() {
             if (count == end) {
                 // 終了
                 clearInterval(id);
-                next();
+                under10();
             }
         }
-        , interval
-    )
+        , 1000)
 }
 
+// 10より小さい数を読み上げる
+function under10() {
+    let count2 = 0; // 繰り返し回数カウント用
+    let end2 = no;  // 繰り返しする回数
 
-function next() {
-    let count2 = 0;
-    let end2 = 5;  // 繰り返したい回数
-    let interval2 = 1000; // 繰り返し処理の実行間隔（ミリ秒数 1000=1秒）
     let id2 = setInterval(
         function () {
-            // --- 繰り返したい処理をここに書く ---
             // カウントを表示する
-            num++;
-
             displayNum();
-
-            src++;
-
+            // 音を鳴らす
+            play(no);
             // 実行回数インクリメント
             count2 += 1;
-
-
-            // 音を鳴らす
-            play10();
-
-
 
             // 終了条件
             if (count2 == end2) {
@@ -94,6 +79,19 @@ function next() {
                 clearInterval(id2);
             }
         }
-        , interval2
-    )
+        , 1000)
+}
+
+// メインプログラム
+// 「始」ボタンが押された時の処理
+function start() {
+    if (input > 10) {
+        no = input;
+        over10();
+    }
+
+    else {
+        no = input;
+        under10(no);
+    }
 }
